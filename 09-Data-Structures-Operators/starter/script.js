45e9bcb0b6b3ba70d8525abde43977954ae32dfe
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-02-12 14:23:12 +0900

    map 생성, object to map, for 사용, boolean key 활용

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -45,43 +45,114 @@ const restaurant = {
   orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
+    }
+    };
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+//   console.log(rest);
+
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = new Date().getHours();
+// console.log(time);
+
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+// console.log(rest.size);
+
+// // rest.clear();
+
+// // rest.set([1, 2], 'Test');
+// // console.log(rest);
+
+// // console.log(rest.get([1, 2]));
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// console.log(rest);
+
+// console.log(rest.get(arr));
+
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+])
+console.log(question);
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+console.log(question.get('question'));
+for(const [key, value] of question) {
+  if(typeof key === 'number') {
+    console.log(`Answer ${key} : ${value}`);
   }
-};
-
-const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
-console.log(ordersSet);
-const ordersArr = [...ordersSet];
-console.log(ordersArr);
-
-console.log(new Set('Jonas'));
-console.log(ordersSet.size);
-console.log(ordersSet.has('Pasta'));
-
-ordersSet.add('Garlic Bread');
-ordersSet.add('Garlic Bread');
-console.log(ordersSet);
-
-ordersArr.push('ab')
-console.log(ordersArr);
-
-ordersSet.delete('Risotto');
-console.log(ordersSet);
-
-// ordersSet.clear();
-// console.log(ordersSet);
-
-
-for(const order of ordersSet) {
-  console.log(order);
 }
 
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffSet = [...new Set(staff)];
-console.log(staffSet);
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
 
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+console.log([...question]);
 
-console.log(new Set('Jonasschmedtman').size);
+// const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
+// console.log(ordersSet);
+// const ordersArr = [...ordersSet];
+// console.log(ordersArr);
+
+// console.log(new Set('Jonas'));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pasta'));
+
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('Garlic Bread');
+// console.log(ordersSet);
+
+// ordersArr.push('ab')
+// console.log(ordersArr);
+
+// ordersSet.delete('Risotto');
+// console.log(ordersSet);
+
+// // ordersSet.clear();
+// // console.log(ordersSet);
+
+
+// for(const order of ordersSet) {
+//   console.log(order);
+// }
+
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffSet = [...new Set(staff)];
+// console.log(staffSet);
+
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+// console.log(new Set('Jonasschmedtman').size);
 
 // const properties = Object.keys(openingHours);
 // console.log(properties);
