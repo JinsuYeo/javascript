ab60e2369f377c0d4eecf66deb8ae183299d272b
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-08 10:58:30 +0900

    Array method(slice, splice, reverse, concat, join, forEach)

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,49 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+/* let arr = ['a', 'b', 'c', 'd', 'e'];
+
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+  
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// JOIN
+console.log(letters.join(' - ')); */
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for(const movement of movements) {
+  if(movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(a => a > 0 ? console.log(`You deposited ${a}`) : console.log(`You withdrew ${Math.abs(a)}`));
