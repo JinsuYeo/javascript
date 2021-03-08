a10288c70a7031c8942e7713ebc37ba6d59688fe
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-08 13:16:05 +0900

    array.forEach() 활용 및 배열 응용 웹앱

---
+++ 11-Arrays-Bankist/starter/challenge.js
@@ -0,0 +1,22 @@
+'use strict';
+/* 
+const julia = [3, 5, 2, 12, 7];
+const kate = [4, 1, 15, 8, 3];
+const julia2 = [9, 16, 6, 8, 3];
+const kate2 = [10, 5, 6, 1, 4];
+
+
+const checkDogs = function(dogsJulia, dogsKate) {
+    const copyJulia = dogsJulia.slice();
+    copyJulia.splice(0, 1);
+    copyJulia.splice(-2, 2);
+
+    const juliaAndKate = copyJulia.concat(dogsKate);
+    
+    juliaAndKate.forEach((dog, i) => console.log(`Dog number ${i} is ${dog >= 3 ? `an audult, and is ${dog} years old` : 'still a puppy 🐶'}`));
+}
+
+checkDogs(julia, kate);
+console.log('-'.padEnd(50, '-'));
+checkDogs(julia2, kate2); 
+*/
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,15 +61,31 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    
+    const html = `<div class="movements__row">
+                    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+                    <div class="movements__date">3 days ago</div>
+                    <div class="movements__value">${mov}€</div>
+                  </div>`
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  })
+}
+displayMovements(account1.movements);
+
+
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -106,14 +122,41 @@ console.log(letters);
 // JOIN
 console.log(letters.join(' - ')); */
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/* const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-for(const movement of movements) {
+for(const [i, movement] of movements.entries()) {
   if(movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
 
-movements.forEach(a => a > 0 ? console.log(`You deposited ${a}`) : console.log(`You withdrew ${Math.abs(a)}`));
+console.log('---- FOREACH ----');
+movements.forEach(function(movement, index, array) {
+  if(movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+// movements.forEach(a => a > 0 ? console.log(`You deposited ${a}`) : console.log(`You withdrew ${Math.abs(a)}`));
+ */
+
+/* const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+// Map
+currencies.forEach(function(value, key, map) {
+  console.log(`${key}: ${value}`);
+})
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, key, set) {
+  console.log(`${key}: ${value}`);
+}) */
