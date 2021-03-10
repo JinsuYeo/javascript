cdb06a60895f3b777dca00957b14f6b5ada45107
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-10 12:42:26 +0900

    Empty array + fill()

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -405,5 +405,14 @@ console.log(movements.filter(deposit)); */
 // console.log(movements.sort((a, b) => a - b));
 // console.log(movements.sort((a, b) => b - a));
 
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
 const x = new Array(7);
 console.log(x);
+// console.log(x.map(() => 5));
+
+x.fill(1, 3);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
