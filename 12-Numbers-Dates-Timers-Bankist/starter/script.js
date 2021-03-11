ccf846f07987a845c5a9c016890bc2a2e4cdb03a
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-11 12:28:44 +0900

    round()와 toFixed의 차이는 숫자를 반환하는가 문자열을 반환하는가임.

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -272,33 +272,35 @@ console.log(Number.isFinite('20'));
 console.log(Number.isFinite(+'20X'));
 console.log(Number.isFinite(20 / 0)); */
 
-console.log(Math.sqrt(25));
-console.log(25 ** (1/2));
-console.log(8 ** (1/3));
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1/2));
+// console.log(8 ** (1/3));
 
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, '23', 11, 2));
-console.log(Math.max(5, 18, '23px', 11, 2));
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, '23', 11, 2));
+// console.log(Math.max(5, 18, '23px', 11, 2));
 
-console.log(Math.min(5, 18, 23, 11, 2));
+// console.log(Math.min(5, 18, 23, 11, 2));
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-console.log(Math.floor(Math.random() * 6) + 1);
+// console.log(Math.floor(Math.random() * 6) + 1);
 
-const randomInt = (min, max) => Math.trunc(Math.random() * (max - min)+ 1) + min;
-console.log(randomInt(10, 20));
+// const randomInt = (min, max) => Math.trunc(Math.random() * (max - min)+ 1) + min;
+// console.log(randomInt(10, 20));
 
-console.log(Math.trunc(23.3));
+// console.log(Math.trunc(23.3));
 
-console.log(Math.round(23.3));
-console.log(Math.round(23.9));
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
 
-console.log(Math.ceil(23.3));
-console.log(Math.ceil(23.9));
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
 
-console.log(Math.floor(23.3));
-console.log(Math.floor('23.9'));
+// console.log(Math.floor(23.3));
+// console.log(Math.floor('23.9'));
 
-console.log(Math.trunc(-23.9));
-console.log(Math.floor(-23.9));
+// console.log(Math.trunc(-23.9));
+// console.log(Math.floor(-23.9));
+
+// console.log((2.7).toFixed(0));
