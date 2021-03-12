37a605ee6199c1a1c9a51fe70c7de9946476b8fd
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-12 10:24:38 +0900

    Date와 메서드들
    new Date(), getFullYear(), getMonth() 등

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -304,3 +304,37 @@ console.log(Number.isFinite(20 / 0)); */
 // console.log(typeof 20n);
 
 // console.log(213123123123n + 'string');
+
+
+/* const now = new Date();
+console.log(now);
+
+console.log(new Date('Fri Mar 12 2021 09:31:59'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+
+ */
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142224580000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
