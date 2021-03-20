cb648832beef27eb50d9f76fcfed8cbc25021313
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-20 11:54:21 +0900

    Event listening의 3가지 방법
    그러나 모던 자바스크립트에서는 항상 addEventListener()사용. 특히 html에서 이벤트 리스너 사용 금지할 것.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -109,26 +109,43 @@ logo.classList.contains('c', 'j');
 // Don't use
 logo.className = 'jonas';   */
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function(e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+// btnScrollTo.addEventListener('click', function(e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+//   console.log(e.target.getBoundingClientRect());
 
-  console.log('current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+//   console.log('current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-  console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+//   console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
 
-  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+//   // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
 
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset, 
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth'
-  // });
+//   // window.scrollTo({
+//   //   left: s1coords.left + window.pageXOffset, 
+//   //   top: s1coords.top + window.pageYOffset,
+//   //   behavior: 'smooth'
+//   // });
 
-  section1.scrollIntoView({behavior: 'smooth'});
-});
+//   section1.scrollIntoView({behavior: 'smooth'});
+// });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function(e) {
+  alert('addEventListener: Great! You are reading the heading! :D')
+}
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000);
+
+/* h1.onmouseenter = function(e) {
+  alert('onmouseenter: Great! You are reading the heading! :D')
+} */
+
