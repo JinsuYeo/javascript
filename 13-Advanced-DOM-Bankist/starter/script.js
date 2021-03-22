d2f212e18e05d157c5ce3e18dff74dc584845d49
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-22 15:46:37 +0900

    DOMContentLoaded

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -459,3 +459,16 @@ logo.className = 'jonas';   */
 //   if(el !== h1) el.style.transform = 'scale(0.5)';
 // })
 
+// document.addEventListener('DOMContentLoaded', function(e) {
+//   console.log(e);
+// });
+
+// window.addEventListener('load', function(e) {
+//   console.log(e);
+// });
+
+// window.addEventListener('beforeunload', function(e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// })
