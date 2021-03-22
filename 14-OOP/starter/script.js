cb31e88e989029024ef3ceb5d4a6f99717c101de
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-22 16:09:49 +0900

    new 연산자, 생성자 함수, instanceof

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    this.calcAge = function() {
+        console.log(2037 - this.birthYear);
+    }
+}
+
+const jonas = new Person('Jonas', 1991);
+
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 2017);
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
