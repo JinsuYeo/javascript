6a0af6ce980f7fd02e88b1724c86e92e1e912988
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-25 11:14:08 +0900

    캡슐화(_, 와 field 기능 이용)
    class field는 아직 구현이 다 되지 않음.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -301,34 +301,59 @@ console.dir(x => x + 1); */
 // jay.introduce();
 // jay.calcAge();
 
+
+// Public fields
+// Private method
+// Public methods
+// Private methods
+// (there is also the static version)
+
 class Account {
+    // 1) Public fields (instance)
+    locale = navigator.language;
+
+    // 2) Private fields (instance)
+    #movements = [];
+    #pin;
+
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
-        this.locale = navigator.language;
+        // protected property
+        this.#pin = pin;
+        // this._movements = [];
+        // this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
+
+    // Public methods
+
     // Public interface
+    getMovements() {
+        return this.#movements;
+    }
+
     deposit(val) {
-        this.movements.push(val);
+        this.#movements.push(val);
     }
 
     withdraw(val) {
         this.deposit(-val);
     }
 
-    approveLoan(val) {
-        return true;
-    }
-
     requestLoan(val) {
-        if(this.approveLoan(val)) {
+        // if(this.#approveLoan(val)) {
+        if(this._approveLoan(val)) {
             this.deposit(val);
             console.log('Loan approved');
         }
+    }
+
+    // Private methods
+    // #approveLoan(val) {
+    _approveLoan(val) {
+        return true;
     }
 }
 
@@ -340,6 +365,8 @@ console.log(acc1);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
+console.log(acc1.getMovements());
 
-console.log(acc1.movements);
-
+// console.log(acc1.#movements);
+// console.log(accq.#pin);
+// console.log(acc1.#approveLoan);
