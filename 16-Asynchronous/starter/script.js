9c3d077750958fd5c06497709a228d619045e727
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-03-31 13:54:04 +0900

    catch(), finally(), 수동으로 에러 발생

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -9,22 +9,22 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('usa');
 // getCountryData('korea (Republic of)');
 
-// const renderCountry = function(data, className = '') {
-//     const html = `
-//             <article class="country ${className}">
-//               <img class="country__img" src="${data.flag}" />
-//               <div class="country__data">
-//                 <h3 class="country__name">${data.name}</h3>
-//                 <h4 class="country__region">${data.region}</h4>
-//                 <p class="country__row"><span>👫</span>${(+data.population / 100000).toFixed(1)}</p>
-//                 <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//                 <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//               </div>
-//             </article>
-//             `
-//         countriesContainer.insertAdjacentHTML('beforeend', html);    
-//         countriesContainer.style.opacity = 1;
-// }
+const renderCountry = function(data, className = '') {
+    const html = `
+            <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 100000).toFixed(1)}</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+              </div>
+            </article>
+            `
+        countriesContainer.insertAdjacentHTML('beforeend', html);    
+        // countriesContainer.style.opacity = 1;
+}
 
 // const getCountryAndNeighbour = function(country) {
 //     const request = new XMLHttpRequest();
@@ -67,3 +67,56 @@ const countriesContainer = document.querySelector('.countries');
 //         }, 1000);
 //     }, 1000);
 // }, 1000);
+
+const renderError = function(msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    // countriesContainer.style.opacity = 1;
+}
+
+const getJSON = function(url, errMsg = 'Something went wrong') {
+    return fetch(url).then(response => {
+        if(!response.ok) 
+          throw new Error(`${errMsg} ${response.status}`);  
+
+        return response.json();
+    })
+}
+
+const getCountryData = function(country) {
+    const request = getJSON(`https://restcountries.eu/rest/v2/name/${country}`, 'Country not found')
+    .then(function(data) {
+        renderCountry(data[0]);
+        const neighbour = data[0].borders[0];
+
+        if(!neighbour) 
+          throw new Error('No neighbour found!');
+
+        return getJSON(`https://restcountries.eu/rest/v2/alpha/${neighbour}`, 'Country not found');
+    }).then(data => {
+        renderCountry(data, 'neighbour');
+        // const neighbour = data.borders[0];
+
+        // if(!neighbour) return;
+
+        // return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    // }).then(function(response) {
+    //     return response.json();
+    // }).then(data => {
+    //     renderCountry(data, 'neighbour');
+    }).catch(err => {
+        console.error(err);
+        renderError(err.message);
+    }).finally(() => countriesContainer.style.opacity = 1);
+}
+
+// const getCountryData = function(country) {
+//     const request = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(response => response.json())
+//     .then(data => renderCountry(data[0]));
+// }
+
+btn.addEventListener('click', function() {
+    getCountryData('portugal');
+})
+
+getCountryData('australia');
