cab77a96d04de32c14249e084b91a3c217cf5b62
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-04-05 14:43:21 +0900

    모듈패턴

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,4 +1,4 @@
-// Importing module
+/* // Importing module
 // import { addToCart, totalPrice as price , tq } from './shoppingCart.js';
 
 // addToCart('bread', 5);
@@ -15,4 +15,30 @@ add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 
-console.log(cart);
+console.log(cart); */
+
+const shoppingCart2 = (function() {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237
+    const totalQuantity = 23;
+
+    const addToCart = function(product, quantity) {
+        cart.push({product, quantity});
+        console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+    };
+
+    const orderStock = function(product, quantity) {
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    }
+})();
+
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
