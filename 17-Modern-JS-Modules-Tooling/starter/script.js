1fcebb9e4e3f9c45576da91a5b4509e16c093016
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-04-05 12:55:58 +0900

    export, import의 방법들
    import는 하나의 모듈에 대해 한번만 사용하는 것이 좋음, 섞어서 사용 안하는 게 좋음, export되는 값은 참조 값(life connection, 값이 변경되면 importing 모듈에서도 값이 변함)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -6,7 +6,13 @@
 console.log('Importing module');
 // console.log(shippingCost);
 
-import * as shoppingCart from './shoppingCart.js';
-shoppingCart.addToCart('bread', 5);
-console.log(shoppingCart.totalPrice);
+// import * as shoppingCart from './shoppingCart.js';
+// shoppingCart.addToCart('bread', 5);
+// console.log(shoppingCart.totalPrice);
 
+import add, {cart} from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
