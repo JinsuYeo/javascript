36467690099193156c3ab91bb77155057f6f8f1e
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-04-05 14:51:56 +0900

    Common JS
    최신은 es6 모듈 시스템 이용, 그러나 아직 Common JS 사용된 곳 많음

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -17,28 +17,38 @@ add('apples', 4);
 
 console.log(cart); */
 
-const shoppingCart2 = (function() {
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237
-    const totalQuantity = 23;
+// const shoppingCart2 = (function() {
+//     const cart = [];
+//     const shippingCost = 10;
+//     const totalPrice = 237
+//     const totalQuantity = 23;
 
-    const addToCart = function(product, quantity) {
-        cart.push({product, quantity});
-        console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
-    };
+//     const addToCart = function(product, quantity) {
+//         cart.push({product, quantity});
+//         console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+//     };
 
-    const orderStock = function(product, quantity) {
-        console.log(`${quantity} ${product} ordered from supplier`);
-    };
+//     const orderStock = function(product, quantity) {
+//         console.log(`${quantity} ${product} ordered from supplier`);
+//     };
 
-    return {
-        addToCart,
-        cart,
-        totalPrice,
-        totalQuantity,
-    }
-})();
+//     return {
+//         addToCart,
+//         cart,
+//         totalPrice,
+//         totalQuantity,
+//     }
+// })();
 
-shoppingCart2.addToCart('pizza', 2);
-console.log(shoppingCart2);
+// shoppingCart2.addToCart('pizza', 2);
+// console.log(shoppingCart2);
+
+
+// Export
+export.addToCart = function(product, quantity) {
+            cart.push({product, quantity});
+            console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+        };
+
+// Import
+const { addToCart } = require('./shoppingCart.js');
