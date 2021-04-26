c00f74451f671aade48cb6188ef8a1f477f16967
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-04-26 13:22:32 +0900

    북마크 구현 및 리팩토링

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -10,6 +10,7 @@ export const state = {
         page: 1,
         resultsPerPage: RES_PER_PAGE,
     }, 
+    bookmarks: [],
 };
 
 export const loadRecipe = async function(id) {
@@ -26,7 +27,12 @@ export const loadRecipe = async function(id) {
             servings: recipe.servings,
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients,
-        };            
+        };
+
+        if(state.bookmarks.some(bookmark => bookmark.id === id))
+            state.recipe.bookmarked = true;
+            else
+            state.recipe.bookmarked = false;
     } catch(err) {
             console.error(`${err} 💥💥💥💥`);
             throw err;
@@ -46,6 +52,7 @@ export const loadSearchResults = async function(query) {
                 image: rec.image_url,
             };
         });
+        state.search.page = 1;
     } catch(err) {
         console.error(`${err} 💥💥💥💥`);
         throw err;
@@ -69,3 +76,30 @@ export const updateServings = function(newServings) {
 
     state.recipe.servings = newServings;
 };
+
+const persistBookmarks = function() {
+    localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+}
+
+export const addBookmark = function(recipe) {
+    state.bookmarks.push(recipe);
+
+    if(recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+    persistBookmarks();
+};
+
+export const deleteBookmark = function(id) {
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+
+    if(id === state.recipe.id) state.recipe.bookmarked = false;
+
+    persistBookmarks();
+}
+
+const init = function() {
+    const storage = localStorage.getItem('bookmarks');
+    if(storage) state.bookmarks = JSON.parse(storage);
+};
+init();
